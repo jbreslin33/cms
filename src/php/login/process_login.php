6f150e9e01e8f2bd054664b8ec984caeb77c17a3
Jim Breslin <jbreslin33@gmail.com>
2018-10-22 11:58:36 -0400

    rm

--- src/php/login/process_login.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include_once(getenv("DOCUMENT_ROOT") . "/php/database/database.php");
-error_log('home you');
-
-$database = new Database;
-$database->query("insert into ages (name) values ('yo')");
-
-?>
